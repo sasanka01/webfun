15cb3588fa5e19dfb3486df16a85c2ad79fdd67c
sasanka <sasanka@Macs-MacBook-Pro.local>
2017-10-19 12:20:43 -0700

    yellow belt web fun

---
+++ js/declare.js
@@ -0,0 +1,27 @@
+var num1 = 1;
+var num2 = -1;
+var num3 = 0.4
+var num4 = -1.5;
+
+var string1 = "andy";
+var string2 = "moss";
+var string3 = "leo";
+
+var bool1 = false;
+var bool2 = true;
+
+var undef = "asd';
+
+console.log ("the first number is :", num1);
+console.log ("the second number is :", num2);
+console.log ("the third number is :", num3);
+console.log ("the fourth number is :", num4);
+
+console.log ("the first string is :", string1);
+console.log ("the second string is :", string2);
+console.log ("the third string is :", string3);
+
+console.log ("first bool is :", bool1);
+console.log ("second bool is:", bool2);
+
+console.log ("undefined value is :",undef);
