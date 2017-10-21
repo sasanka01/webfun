ead93ba2fb697841008498bdd73d8783958313e6
sasanka <sasanka@Macs-MacBook-Pro.local>
2017-10-20 22:53:25 -0700

    arrayfor.js

--- js/arrayfor.js
+++ js/arrayfor.js
@@ -1,6 +1,7 @@
-var arr = [ "James", "Jill", "Jane", "Jack" ];
-
-for (var i = 0; i < arr.length; i++) {
-
-  console.log(i + " -> " + arr[i]);
-}
+var arr = ["James", "Jill", "Jane", "Jack"];
+    function fancyPrint(array){
+      for (var i = 0; i < arr.length; i++) {
+        console.log(i + ' -> ' + arr[i]);
+      }
+    }
+fancyPrint(arr);
