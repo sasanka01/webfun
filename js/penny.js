def67934dfc265473f4ce52d07d5634cecf64661
sasanka <sasanka@Macs-MacBook-Pro.local>
2017-10-20 22:02:18 -0700

    penny

--- js/penny.js
+++ js/penny.js
@@ -1,14 +1,9 @@
 var earnings = .01;
 var amount = .01;
-for (var i = 2; i <= 100000; i++) {
+for (var i = 2; i <= 30; i++) {
 			earnings = earnings * 2
-			console.log("earnings", earnings);
+			// console.log("earnings", earnings);
 			amount = amount + earnings
 			console.log("amount", amount);
-			if (amount == Infinity) {
-				console.log(i)
-				break
-
 
 			}
-}
