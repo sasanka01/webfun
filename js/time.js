4347eee8a2a812b894d605a40b89efc988c9ebc1
sasanka <sasanka@Macs-MacBook-Pro.local>
2017-10-20 21:04:24 -0700

    time

--- js/time.js
+++ js/time.js
@@ -1,5 +1,24 @@
-var HOUR = 8;
-var MINUTE = 50;
-var PERIOD = "AM";
 
-console.log("it's almost 9 in the morning");
+var HOUR = 8;
+var MINUTE = 10;
+var PERIOD = "AM"
+
+
+if (MINUTE > 30 && PERIOD == "AM")
+{
+  var str = "it's  " + (HOUR + 1) + " in the morning"
+}
+
+else
+{
+  var str = ("it's almost " + (HOUR + 1) + " in the morning")
+}
+
+
+
+  // var str = "it's in the evening."
+// }else{
+  // var str = "it's in the morning."
+// }
+
+console.log(str)
