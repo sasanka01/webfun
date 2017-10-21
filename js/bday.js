07235420603111e9303504b368cfa53baa13e0f6
sasanka <sasanka@Macs-MacBook-Pro.local>
2017-10-20 21:28:43 -0700

    bday

---
+++ js/bday.js
@@ -0,0 +1,12 @@
+var daysUntilMyBirthday = 0;
+if (daysUntilMyBirthday > 30) {
+console.log(daysUntilMyBirthday, "days until my birthday. Such a long time")
+}
+else if (daysUntilMyBirthday < 30 > 1) {
+  console.log(daysUntilMyBirthday, "let roll")
+}
+else {
+  console.log('♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪ღ♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•*'+
+              '♪ღ♪░H░A░P░P░Y░ B░I░R░T░H░D░A░Y░░♪ღ♪ *•♪ღ'+
+              '♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•«')
+}
