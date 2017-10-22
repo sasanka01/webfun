6a35deb8c18eaa12a62b31270acd88591743324c
sasanka <sasanka@Macs-MacBook-Pro.local>
2017-10-22 11:24:00 -0700

    typeof

--- js/num.js
+++ js/num.js
@@ -1,12 +1,12 @@
-function num() {
+function num(arr) {
+    var arr1 = [];
+    for (var i = 0; i < arr.length; i++) {
+      if (typeof arr[i] == "number" ) {
+          arr1.push(arr1[i])
 
-  var arr = numbersOnly([1, "apple", -3, "orange", 0.5]);
-  var arr1 = [];
-  for (var i = 0; i < array.length; i++) {
-    if (typeof arr[i] === "number" ) {
-        arr1 == push.arr;
-
+      }
     }
+  console.log(arr1);
+  return arr1;
   }
-console.log(arr1);
-}
+  num([1, "apple", -3, "orange", 0.5]);
