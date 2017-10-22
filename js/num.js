ad831c4048550092884ef9329ac2d815450e1239
sasanka <sasanka@Macs-MacBook-Pro.local>
2017-10-22 11:29:07 -0700

    typeof

--- js/num.js
+++ js/num.js
@@ -1,12 +1,11 @@
 function num(arr) {
-    var arr1 = [];
+    var container = [];
     for (var i = 0; i < arr.length; i++) {
       if (typeof arr[i] == "number" ) {
-          arr1.push(arr1[i])
-
+          container.push(arr[i])
       }
     }
-  console.log(arr1);
-  return arr1;
+  console.log(container);
+  return container;
   }
   num([1, "apple", -3, "orange", 0.5]);
