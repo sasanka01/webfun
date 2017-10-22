85151a210b3a196466140eec2e5146de8e655b22
sasanka <sasanka@Macs-MacBook-Pro.local>
2017-10-22 11:06:56 -0700

    typeof

---
+++ js/num.js
@@ -0,0 +1,12 @@
+function num() {
+
+  var arr = numbersOnly([1, "apple", -3, "orange", 0.5]);
+  var arr1 = [];
+  for (var i = 0; i < array.length; i++) {
+    if (typeof arr[i] === "number" ) {
+        arr1 == push.arr;
+
+    }
+  }
+console.log(arr1);
+}
